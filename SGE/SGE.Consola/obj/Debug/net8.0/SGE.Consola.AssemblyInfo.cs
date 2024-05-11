bde7a25c109ac103d8d1d685994d9ac23a3b93f0
LucianoLoyola <lucianonicolasloyola@alu.ing.unlp.edu.ar>
2024-05-11 11:11:23 -0300

    merge

--- SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
+++ SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c8d11562ac67c9a34f902f371ab5affa1ce593c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b166d14d4b5e34b7a5c95ecee443341ed75eefb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
