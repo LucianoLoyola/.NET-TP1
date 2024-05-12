6371eb1262230615731438b86b5dd82e159d476c
LucianoLoyola <lucianonicolasloyola@alu.ing.unlp.edu.ar>
2024-05-12 17:55:00 -0300

    Mejora en código de program consola

--- SGE/SGE.Consola/Program.cs
+++ SGE/SGE.Consola/Program.cs
@@ -16,19 +16,24 @@ var EliminarTramite=new CasoDeUsoTramiteBaja(repoT);
 var ModificarTramite=new CasoDeUsoTramiteModificacion(repoT);
 
 //creo los expedientes
-Expediente exp = new Expediente() {caratula="Random"};
-Expediente exp2 = new Expediente() {caratula="Prueba"};
+Expediente exp = new Expediente() {caratula="Expediente N° 1"};
+Expediente exp2 = new Expediente() {caratula="Expediente N° 2"};
+Expediente exp3 = new Expediente() {caratula="Expediente N° 3"};
 
 //Creo los trámites
-Tramite tra1=new Tramite() {Contenido="Test", ExpedienteId=40};
-Tramite tra2=new Tramite() {Contenido="Xes", ExpedienteId=41};
-Tramite tra3=new Tramite() {Contenido="Res", ExpedienteId=42};
-Tramite tra4=new Tramite() {Contenido="Mes", ExpedienteId=43};
+Tramite tra1=new Tramite() {Contenido="Big Iron", ExpedienteId=exp.Id};
+Tramite tra2=new Tramite() {Contenido="Blue Moon", ExpedienteId=exp.Id};
+Tramite tra3=new Tramite() {Contenido="It's a Sin", ExpedienteId=exp2.Id};
+Tramite tra4=new Tramite() {Contenido="Johnny Guitar", ExpedienteId=exp2.Id};
+Tramite tra5=new Tramite() {Contenido="Saddle Tramp", ExpedienteId=exp3.Id};
+Tramite tra6=new Tramite() {Contenido="Atom Bomb Baby", ExpedienteId=exp3.Id};
 List<Tramite> listaDeTramites = new List<Tramite>();
 listaDeTramites.Add(tra1);
 listaDeTramites.Add(tra2);
 listaDeTramites.Add(tra3);
 listaDeTramites.Add(tra4);
+listaDeTramites.Add(tra5);
+listaDeTramites.Add(tra6);
 
 //ejecuto los casos de uso con validaciones 
 try
@@ -36,10 +41,13 @@ try
     //Los casos de uso deben recibir el ID del usuario. La validación provisoria dice que al recibir '1', lo valida como correcto. Leer 'IServicioAutorizacion' y provisorio
     AgregarExpediente.Ejecutar(exp,exp.Id,1);
     AgregarExpediente.Ejecutar(exp2,exp2.Id,1);
+    AgregarExpediente.Ejecutar(exp3,exp3.Id,1);
     AgregarTramite.Ejecutar(tra1,exp.Id,1);
     AgregarTramite.Ejecutar(tra2,exp2.Id,1);
     AgregarTramite.Ejecutar(tra3,exp.Id,1);
     AgregarTramite.Ejecutar(tra4,exp2.Id,1);
+    AgregarTramite.Ejecutar(tra5,exp3.Id,1);
+    AgregarTramite.Ejecutar(tra6,exp3.Id,1);
     Console.WriteLine("------------------------------------------");
     Console.WriteLine("Por eliminar tramite con ID: "+tra2.Id);
     EliminarTramite.Ejecutar(tra2.Id,1);
@@ -58,8 +66,8 @@ try
     }
 
     Console.WriteLine("------------------------------------------");
-    //Console.WriteLine("Por eliminar expediente con ID: "+exp.Id);
-    //EliminarExpediente.Ejecutar(exp.Id,1,listaDeTramites,EliminarTramite);
+    Console.WriteLine("Por eliminar expediente con ID: "+exp.Id);
+    EliminarExpediente.Ejecutar(exp.Id,1,listaDeTramites,EliminarTramite);
     Console.WriteLine("------------------------------------------");
     Console.WriteLine("Por modificar tramite con ID: "+tra1.Id);//ESTA FUNCIONANDO MAL
     tra1.Contenido="Cambio de contenido";
--- SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
+++ SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+306cda4621d7cfaebe70757b3e8ab3d9f210c14b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97a268311f3f93b02ffae23f31aff18e37a709ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
