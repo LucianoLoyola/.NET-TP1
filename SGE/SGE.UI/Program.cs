bc08cfff0d77ebae374032b7b0ccbd56a207c655
LucianoLoyola <lucianonicolasloyola@alu.ing.unlp.edu.ar>
2024-06-21 13:44:32 -0300

    Login realizado

--- SGE/SGE.UI/Program.cs
+++ SGE/SGE.UI/Program.cs
@@ -1,42 +1,53 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
 using SGE.UI.Components;
-
 using SGE.Repositorios;
 using SGE.Aplicacion;
 
-ProyectoSqlite bd = new ProyectoSqlite();
-ITramiteRepositorio repoT = new RepositorioTramiteTXT();
+// Crear una instancia de DbContextOptions para UserAccountContext
+var connectionString = "Data Source=UserAccount.sqlite";
+var options = new DbContextOptionsBuilder<UserAccountContext>()
+    .UseSqlite(connectionString)
+    .Options;
 
-ProyectoSqlite.Inicializar();
-using (var context = new UserAccountContext())
+// Inicializar la base de datos si es necesario
+ProyectoSqlite.Inicializar(options);
+
+// Usar el contexto para mostrar los datos de UserAccount
+using (var context = new UserAccountContext(options))
 {
     Console.WriteLine("-- Tabla UserAccount --");
     foreach (var a in context.UserAccount)
     {
-    Console.WriteLine($"{a.Id} {a.UserName}");
+        Console.WriteLine($"{a.Id} {a.UserName}");
     }
 }
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configurar servicios y middleware
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
-        options.Cookie.Name="Auth_token";
-        options.LoginPath="/login";
-        options.Cookie.MaxAge=TimeSpan.FromMinutes(30);
-        options.AccessDeniedPath="/access-denied";
+        options.Cookie.Name = "auth_token";
+        options.LoginPath = "/login";
+        options.Cookie.MaxAge = TimeSpan.FromMinutes(30);
+        options.AccessDeniedPath = "/access-denied";
     });
+
 builder.Services.AddAuthorization();
 builder.Services.AddCascadingAuthenticationState();
 
+// Agregar el DbContext de UserAccountContext con la configuración de SQLite
+builder.Services.AddDbContext<UserAccountContext>(options =>
+    options.UseSqlite(connectionString));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configurar el pipeline de solicitudes HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
