7a5c3306c490ff2d7eda42bd6d6c22ddbdf60d77
Gabriel Torres <111906750+gabitorres2@users.noreply.github.com>
2024-07-05 22:38:59 -0300

    upd

---
+++ SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoActualizarPerfil.cs
@@ -0,0 +1,13 @@
+﻿namespace SGE.Aplicacion.CasosDeUso;
+using SGE.Aplicacion.Entidades;
+using SGE.Aplicacion.Interfaces;
+public class CasoDeUsoActualizarPerfil(IRepositorioUsuario repositorio):UserAccountUseCase(repositorio)
+{
+    public void Ejecutar(UserAccount userAccount)
+    {
+    //aquí podríamos insertar código de validación de cliente
+
+        Repositorio.ModificarUsuario(userAccount);
+    }
+}
+
---
+++ SGE/SGE.Aplicacion/Entidades/ViewModels/UpdateViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SGE.Aplicacion.ViewModels
+{
+    public class UpdateViewModel
+    {
+        public string? UserName { get; set; }
+        
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public string? Email { get; set; }
+        
+        [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres")]
+        public string? Password { get; set; }
+        
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden")]
+        public string? ConfirmPassword { get; set; }
+        
+        public string? Role { get; set; }  // Si los roles son predefinidos, considera usar un dropdown en el formulario
+    }
+}
