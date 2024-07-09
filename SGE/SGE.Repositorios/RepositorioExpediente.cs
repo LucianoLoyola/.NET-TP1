ff74f5e97d5e30df9c4ac7bab89352bb291f849a
JoacoRodriguez9 <joaquirodri2000@gmail.com>
2024-07-08 22:53:31 -0300

    excepcion-repositorios

--- SGE/SGE.Repositorios/RepositorioExpediente.cs
+++ SGE/SGE.Repositorios/RepositorioExpediente.cs
@@ -14,7 +14,7 @@ public class RepositorioExpediente : IRepositorioExpediente
     }
 
     public void AgregarExpediente(Expediente expediente, int idUsuario){
-        if (expediente.caratula == null) throw new Exception("El expediente debe tener una caratula");
+        if (expediente.caratula == null) throw new RepositorioException("El expediente debe tener una caratula");
         db.Add(expediente);
         db.SaveChanges();
     }
@@ -23,7 +23,7 @@ public class RepositorioExpediente : IRepositorioExpediente
         //La busca por Id
         var exp_existente = db.Expedientes.Find(expediente.Id);
 
-        if (exp_existente == null) throw new Exception("No se encontro un expediente con ese id\n");
+        if (exp_existente == null) throw new RepositorioException("No se encontro un expediente con ese id\n");
 
         exp_existente.caratula = expediente.caratula;
         exp_existente.fechaHoraCreacion = expediente.fechaHoraCreacion;
@@ -35,75 +35,25 @@ public class RepositorioExpediente : IRepositorioExpediente
         db.SaveChanges();
     }
 
-    // public void AgregarTramiteExp(int IdExp, Tramite tramite) {
-    //     var exp_existente = db.Expedientes.Find(IdExp);
-
-    //     if (exp_existente == null) throw new Exception("No se encontro un expediente con ese id\n");
-
-    //     exp_existente.listaTramites = expediente.listaTramites;
-
-    //     db.SaveChanges();
-
-    // }
-
-
-    // public void ModificarTramiteDeLista(Expediente expediente, Tramite tramiteModificado){ //método para modificar lista de trámites
-    //     if (expediente.listaTramites != null){
-    //         for (int i = 0; i < expediente.listaTramites.Count; i++){
-    //             if (expediente.listaTramites[i].Id == tramiteModificado.Id){
-    //                 expediente.listaTramites[i] = tramiteModificado;
-    //             }
-    //         }
-    //     }
-    // }
-    // public void EliminarTramiteDeLista(Expediente expediente, int tramiteId){//método para eliminar un tramite de la lista
-    //     if (expediente.listaTramites != null){
-    //         for (int i = 0; i < expediente.listaTramites.Count; i++){
-    //             if (expediente.listaTramites[i].Id == tramiteId){
-    //                 expediente.listaTramites.RemoveAt(i);
-    //             }
-    //         }
-    //     }
-    // }
-
-    // public void AgregarTramiteALista(Expediente expediente, Tramite nuevoTramite){//método para agregar un tramite a la lista
-    //     if (expediente.listaTramites == null){
-    //         expediente.listaTramites = new List<Tramite>();
-    //     }
-    //     expediente.listaTramites.Add(nuevoTramite);
-    // }
-
-    // public void EliminarTodosLosTramites(){
-    //     if (listaTramites != null){
-    //         listaTramites.Clear(); // Elimina todos los trámites de la lista
-    //     }
-    // }
-
-
-
-
-
-
-
-
-
-
-
 
     public List<Expediente> ListarExpedientes(){
-        return db.Expedientes.ToList();
+        var lista = db.Expedientes.ToList();
+        if (lista == null) throw new RepositorioException("No existen expedientes");
+        return lista;
     }
 
     public void EliminarExpediente(int idExpediente){
         var exp_existente = db.Expedientes.Find(idExpediente);
-        if (exp_existente == null) throw new Exception("No se encontro el expediente con ese id");
+        if (exp_existente == null) throw new RepositorioException("No se encontro el expediente con ese id");
 
         db.Remove(exp_existente); //se borra realmente con el db.SaveChanges()
         db.SaveChanges();
     }
 
-    public Expediente GetExpedientePorId(int id){
-        return db.Expedientes.Where(e => e.Id == id).SingleOrDefault();
+    public Expediente GetExpedientePorId(int id){     
+        var expediente = db.Expedientes.Where(e => e.Id == id).SingleOrDefault();
+        if (expediente == null) throw new RepositorioException("No se encontro expediente con ese id");
+        return expediente;
     }
 
 }
